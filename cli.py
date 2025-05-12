3eae067df5e4a453ea769c0b947ecdfbe51940c2
Rodney Thomas <wj.rodneyt@yahoo.com>
2025-05-12 13:24:15 -0500

    CLI

--- cli.py
+++ cli.py
@@ -1,16 +1,33 @@
 import cmd
+import socket
+
 
 class CLI(cmd.Cmd):
     prompt = '>> '
     intro = 'Welcome to WARJORN CLI. Type "help" for available commands.'
 
     def do_hello(self, line):
-        # Print Greeting
+        """
+        Print 'Hello, World!'.
+        """
         print("Hello, World!")
 
     def do_quit(self, line):
-        # Exit CLI
+        """
+        Quit the CLI.
+        """
         return True
+
+    def do_network(self, line):
+        """
+        Displays General Information on Your Network.
+        """
+        print("    Information on your network.")
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        print(f"    > Hostname: {hostname}")
+        print(f"    > IP Address: {ip_address}")
+
 
 if __name__ == '__main__':
     CLI().cmdloop()
